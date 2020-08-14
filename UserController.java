e582242a83c53479d163265026499bd2ee4f9446
maritn0054 <maritn0054@gmail.com>
2020-08-14 18:30:59 +0900

    Add UserController[update]

--- UserController.java
+++ UserController.java
@@ -8,7 +8,19 @@ public class UserController{
 		return "user/login";
 	}
 
-	
+	//회원 추가 페이지로 이동
+	@RequestMapping("/main/insert")
+	public String insert(){
+		logger.info("경로 : insert");
+		return "user/insert";
+	}
+
+	//변경 페이지로 이동
+	@RequestMapping("/main/update")
+	public String update(){
+		logger.info("경로 : update");
+		return "user/update";
+	}
 
 }
 
